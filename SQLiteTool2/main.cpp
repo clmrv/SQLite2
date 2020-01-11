5b72be24869baac32c87fb5336da77baed4f3635
clmrv <everay@wp.pl>
2020-01-11 21:11:25 +0100

    Initial Commit

---
+++ SQLiteTool2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SQLiteTool2
+//
+//  Created by celmer on 11/01/2020.
+//  Copyright © 2020 celmer. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
